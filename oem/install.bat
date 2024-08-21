b472be2d0a2def30da75877653ef38f0180ea587
osnmoh007 <112149956+osnmoh007@users.noreply.github.com>
2024-08-21 01:22:48 +0100

    Update install.bat

--- oem/install.bat
+++ oem/install.bat
@@ -1,1 +1,11 @@
+@echo off
+
+:: Disable AutoAdminLogon
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 0 /f
+
+:: Clear Default UserName and Password
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /f
+
+:: Optionally clear Default Domain Name
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /f
